e80a87d0887a4ae9dfa4575eff9ecccc489307a1
sathira <sathira@minthrm.com>
2020-02-20 07:45:24 +0530

    first commit

---
+++ shareBooks.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home | BookStore</title>
+
+
+    
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Open+Sans:300,400,600|Roboto+Condensed:300,400,700|Roboto:100,300,400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister&display=swap" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="resources/css/bootstrap.min.css" rel="stylesheet">
+    <link href="resources/css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+   
+    <!--Start of Header Row-->
+    <div id="header-row">
+      
+      <!--Start of Header-->
+        <div id="header" class="container">         
+          <div class="row">
+            
+          <!--Start of Header Left-->
+            <div id="header-left" class="col-md-4 col-sm-4 hidden-xs">
+              <a href="#" title="Home | BookStore"><img class="img-responsive" src="resources/images/logo_dark.png" alt="BookStore Logo"></a>
+            </div>
+          <!--End of Header Left-->
+
+          <!--Start of Header Right/Navigation-->
+            <nav id="header-right" class="navbar navbar-default col-md-8 col-sm-8" role="navigation">
+            <div class="row">
+    
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar icon-bar1"></span>
+                <span class="icon-bar icon-bar2"></span>
+                <span class="icon-bar icon-bar3"></span>
+                </button>
+                <a class="navbar-brand hidden-lg hidden-md hidden-sm" href="#" title="Home | BookStore"><img src="resources/images/dimensions-it-logo-mini.png" alt="Dimensions IT Logo"></a>
+              </div>
+
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                  <li ><a href="#">Home </a></li>
+                  <li class="active"><a href="#">Share Books</a></li>
+                  <li><a href="#">Chat Room</a></li>
+                  <li><a href="#">Cart</a></li>
+
+                </ul>
+              </div>
+
+          </div>
+        </nav>
+          <!--End of Header Right/Navigation-->
+
+          </div>
+        </div>
+      <!--End of Header-->
+
+    </div>
+    <!--End of Header Row-->
+<!------------------------------------------------------------------------------------------------------------>
+
+<div class="search-sra">
+    
+<div class="form-group row">
+   <div class="col">
+      <div class="input-group input-group-lg">
+        <input type="text" class="form-control" placeholder="Search By Name">
+            <div class="input-group-btn">
+            <button class="btn btn-default" type="submit">
+                <i class="glyphicon glyphicon-search"></i>
+            </button>
+            </div>
+        </div>
+    </div>
+</div>
+ 
+<div class="form-group row">
+   <div class="col">
+      <div class="input-group input-group-lg">
+        <input type="text" class="form-control" placeholder="Search By Author">
+            <div class="input-group-btn">
+            <button class="btn btn-default" type="submit">
+                <i class="glyphicon glyphicon-search"></i>
+            </button>
+            </div>
+        </div>
+    </div>
+</div>    
+       
+</div>
+
+<div class="show-sra">        
+      <div id="middle-content" class="container">
+        <div class="row">
+        <div class="col-md-4 col-sm-4 col-xs-6 box-def grid">
+          <figure class="effect-ruby">
+            <img class="img-responsive" src="resources/images/about-dimensions-it.jpg" alt="About Dimensions-IT">
+            <figcaption>
+              <h2>About <span>Us</span></h2>
+              <p>What is Dimensions IT?. Read More About Us.</p>
+              <a href="#">View more</a>
+            </figcaption>     
+          </figure>
+        </div>      
+    </div>
+   </div>
+</div>
+
+
+  </body>
+</html>
