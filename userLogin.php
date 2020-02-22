8068d6f421d69cb937337f2fce4411d7441691dd
Brendon <brendon@minthrm.com>
2020-02-22 22:32:40 +0530

    user login added

---
+++ userLogin.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home | BookStore</title>
+
+
+    
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Open+Sans:300,400,600|Roboto+Condensed:300,400,700|Roboto:100,300,400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Love+Ya+Like+A+Sister&display=swap" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="resources/css/bootstrap.min.css" rel="stylesheet">
+    <link href="resources/css/custom/user_login.css" rel="stylesheet">
+    <link href="resources/css/slider.css" rel="stylesheet">
+     <noscript>
+       <link rel="stylesheet" type="text/css" href="resources/css/nojs.css" />
+         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+     </noscript>   
+    <link rel="stylesheet" type="text/css" href="resources/css/hover/normalize.css" />
+    <!-- <link rel="stylesheet" type="text/css" href="resources/css/hover/demo.css" /> -->
+    <link rel="stylesheet" type="text/css" href="resources/css/hover/set1.css" />
+    <link rel="stylesheet" type="text/css" href ="resources/css/jquery.openCarousel.css">
+
+
+    <!-- Testimonials -->
+    <!-- <link rel="stylesheet" href="resources/testimonials/css/reset.css"> --> <!-- CSS reset -->
+    <link rel="stylesheet" href="resources/testimonials/css/style.css"> <!-- Resource style -->
+    <script src="resources/testimonials/js/modernizr.js"></script> <!-- Modernizr -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Testimonials -->
+
+
+    <link href="resources/css/style.css" rel="stylesheet">
+
+    <script type="text/javascript" src="resources/js/modernizr.custom.28468.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
+  <body>
+   
+    <!--Start of Header Row-->
+    <div id="header-row">
+      
+      <!--Start of Header-->
+        <div id="header" class="container">         
+          <div class="row">
+            
+          <!--Start of Header Left-->
+            <div id="header-left" class="col-md-4 col-sm-4 hidden-xs">
+              <a href="#" title="Home | BookStore"><img class="img-responsive" src="resources/images/logo_dark.png" alt="BookStore Logo"></a>
+            </div>
+          <!--End of Header Left-->
+
+          <!--Start of Header Right/Navigation-->
+            <nav id="header-right" class="navbar navbar-default col-md-8 col-sm-8" role="navigation">
+            <div class="row">
+    
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar icon-bar1"></span>
+                <span class="icon-bar icon-bar2"></span>
+                <span class="icon-bar icon-bar3"></span>
+                </button>
+                <a class="navbar-brand hidden-lg hidden-md hidden-sm" href="#" title="Home | BookStore"><img src="resources/images/dimensions-it-logo-mini.png" alt="Dimensions IT Logo"></a>
+              </div>
+
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                  <li class="active"><a href="#">Home </a></li>
+                  <li><a href="#">Share Books</a></li>
+                  <li><a href="#">Chat Room</a></li>
+                  <li><a href="#">Cart</a></li>
+
+                </ul>
+              </div>
+
+          </div>
+        </nav>
+          <!--End of Header Right/Navigation-->
+
+          </div>
+        </div>
+      <!--End of Header-->
+
+    </div>
+    <!--End of Header Row-->
+<!------------------------------------------------------------------------------------------------------------>
+
+<!-- Start User Login-->
+    <div class ="container">
+        <div class="signup-form col-sm-6">
+            <form action="/examples/actions/confirmation.php" method="post">
+                <div class="text-center">
+                    <h2>Register</h2>
+<!--                    <p class="hint-text">Create your account. It's free and only takes a minute.</p>-->
+                </div>
+
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-xs-6"><input type="text" class="form-control" name="first_name" placeholder="First Name" required="required"></div>
+                        <div class="col-xs-6"><input type="text" class="form-control" name="last_name" placeholder="Last Name" required="required"></div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <input type="email" class="form-control" name="email" placeholder="Email" required="required">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" name="password" placeholder="Password" required="required">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required="required">
+                </div>
+                <div class="form-group">
+                    <label class="checkbox-inline"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-success btn-lg btn-block">Register Now</button>
+                </div>
+            </form>
+            <div class="text-center">Already have an account? <a href="#">Sign in</a></div>
+        </div>
+
+    </div>
+
+<!--End User Login-->
+
+    <!--Start of Copyright Row-->
+    <div id="copyright-row">
+
+      <!--Start of Copyright-->
+      <div id="copyright" class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <p><span>BookStore</span> &copy; 2020 | All Rights Reserved</p>
+          </div>
+        </div>
+      </div>
+      <!--Start of Copyright-->
+
+    </div>
+    <!--Start of Copyright Row-->
+
+<!------------------------------------------------------------------------------------------------------------>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script
+  src="https://code.jquery.com/jquery-1.12.4.min.js"
+  integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+  crossorigin="anonymous"></script>
+    <script src="resources/js/bootstrap.min.js"></script>
+
+    <!-- Testimonials -->
+    <!-- <script src="resources/testimonials/js/jquery-2.1.1.js"></script> -->
+    <script src="resources/testimonials/js/masonry.pkgd.min.js"></script>
+    <script src="resources/testimonials/js/jquery.flexslider-min.js"></script>
+    <script src="resources/testimonials/js/main.js"></script> 
+    <!-- Testimonials -->
+
+    <script type="text/javascript" src="resources/js/jquery.cslider.js"></script>
+
+    <script type="text/javascript">
+     $(function() {
+      
+        $('#da-slider').cslider({
+          autoplay  : true,
+          bgincrement : 450
+        });
+
+
+      $window = $(window);
+      var $scroll = $('#bottom-full');               
+      $(window).scroll(function() {                         
+        var yPos = -($window.scrollTop() / $scroll.data('speed')); 
+        var coords = '50% '+ yPos + 'px';
+        $scroll.css({ backgroundPosition: coords });    
+      }); 
+
+      });
+    </script> 
+
+
+
+  </body>
+
+
+</html>
